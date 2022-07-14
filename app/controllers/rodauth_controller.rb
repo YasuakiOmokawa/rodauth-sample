6b2f9337b16d18ecf2eedb7fa424c47408ed7733
面川泰明 <omokawa@senk-inc.co.jp>
2022-07-14 14:29:00 +0900

    feat: change layout in case request_path

--- app/controllers/rodauth_controller.rb
+++ app/controllers/rodauth_controller.rb
@@ -1,4 +1,21 @@
 class RodauthController < ApplicationController
   # used by Rodauth for rendering views, CSRF protection, and running any
   # registered action callbacks and rescue_from handlers
+  layout :rodauth_layout
+
+  private
+
+  def rodauth_layout
+    case request.path
+    when rodauth.login_path,
+         rodauth.create_account_path,
+         rodauth.verify_account_path,
+         rodauth.verify_account_resend_path,
+         rodauth.reset_password_path,
+         rodauth.reset_password_request_path
+      "authentication"
+    else
+      "dashboard"
+    end
+  end
 end
