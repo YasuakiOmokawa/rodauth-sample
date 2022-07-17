493a37db9a45581bf7e3dbc62e963aff79b6707d
面川泰明 <omokawa@senk-inc.co.jp>
2022-07-17 18:12:08 +0900

    feat: enable email link in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,4 +72,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.x.default_url_options.protocol = 'http'
+  config.x.default_url_options.host = 'localhost:3001'
 end
