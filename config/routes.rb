43d2f6020954efb4a5a88feae2359400a508d0c9
面川泰明 <omokawa@senk-inc.co.jp>
2022-07-21 12:29:13 +0900

    feat: fix root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "homes#index"
 
   draw :debug
 end
