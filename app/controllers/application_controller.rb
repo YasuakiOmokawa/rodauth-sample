2e9af94ed9521da3235f6f39fdaa324b350b8bbf
面川泰明 <omokawa@senk-inc.co.jp>
2022-07-23 17:35:58 +0900

    feat: add authenticate before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    rodauth.require_authentication # redirect to login page if not authenticated
+  end
 end
