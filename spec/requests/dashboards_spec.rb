d671d4348061dfc649822f02e7aa5795ddd7caa2
面川泰明 <omokawa@senk-inc.co.jp>
2022-07-24 18:43:29 +0900

    ログインしてない場合のテストが通過することを確認

--- spec/requests/dashboards_spec.rb
+++ spec/requests/dashboards_spec.rb
@@ -1,31 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "/dashboard", type: :request do
+RSpec.describe DashboardsController, type: :request do
   describe "GET /show" do
     context 'ログインしている場合' do
       before do
+        get dashboard_path
         account = Account.create!(email: "user@example.com", password: "secret", status: "verified")
         login(account)
         get dashboard_path
       end
 
-      it { is_expected.to have_http_status(:ok) }
+      it 'ダッシュボードへ遷移する' do
+        expect(response).to have_http_status(:ok)
+      end
 
       private
 
-      # Manually modify the session into what Rodauth expects.
       def login(account)
-        session[:account_id] = account.id
-        session[:authenticated_by] = ["password"] # or ["password", "totp"] for MFA
+        binding.b
+        rodauth.scope.session[:account_id] = account.id
+        rodauth.scope.session[:authenticated_by] = ["password"] # or ["password", "totp"] for MFA
+      end
+
+      def logout
+        session.clear
       end
     end
 
     context 'ログインしてない場合' do
       before { get dashboard_path }
 
-      binding.b
-      it { is_expected.to have_http_status(:found) }
-      it { is_expected.to redirect_to rodauth.login_path }
+      it 'ログイン画面へリダイレクト' do
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to rodauth.login_path
+      end
     end
   end
 end
