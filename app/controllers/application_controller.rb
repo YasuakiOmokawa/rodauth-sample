c88a34046a9cbb5fcb4cc407d1176474a28f3de1
面川泰明 <omokawa@senk-inc.co.jp>
2022-07-30 15:08:34 +0900

    feat: remove break point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate
-    binding.b
     rodauth.require_authentication # redirect to login page if not authenticated
   end
 end
