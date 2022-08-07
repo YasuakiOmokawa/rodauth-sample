8b9ea7cc07b2300e25a83f3cb70f74066a81fc27
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-07 15:51:00 +0900

    feat: add case prepare when argument class

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,17 @@ class Trip
   attr_reader :bicycles, :customers, :vehicle
 
   # 'mechanic' accepts any class type
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(prepares)
+    prepares.each do |prepare|
+      case prepare
+      when Mecanic
+        prepare.prepare_bicycles(bicycles)
+      when TripCordinator
+        prepare.buy_food(customers)
+      when Driver
+        prepare.gas_up(vehicle)
+        prepare.fill_water_tank(vehicle)
+      end
+    end
   end
 end
