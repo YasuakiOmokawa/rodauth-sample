8f3a8eb3aa4e8247da4f5b190d1cfacbac67a7dd
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-08 14:40:03 +0900

    feat: call prepare_trip(self)
    prepare_trip is duck.
    duck means abstract interface.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,15 +4,7 @@ class Trip
   # 'mechanic' accepts any class type
   def prepare(prepares)
     prepares.each do |prepare|
-      case prepare
-      when Mecanic
-        prepare.prepare_bicycles(bicycles)
-      when TripCordinator
-        prepare.buy_food(customers)
-      when Driver
-        prepare.gas_up(vehicle)
-        prepare.fill_water_tank(vehicle)
-      end
+      prepare.prepare_trip(self)
     end
   end
 end
