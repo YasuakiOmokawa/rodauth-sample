d63c64e2194c177f04c35d652e649491deb27491
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-11 16:25:52 +0900

    feat: rails db:migrate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,3 +3,14 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
