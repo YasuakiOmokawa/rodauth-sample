398c9e6162fdc3abd4fe0f9b6cae313dd04052ba
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-13 11:54:06 +0900

    feat: include Schedulable

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,17 +1,7 @@
 class Bicycle
-  def initialize(args = {})
-    @schedule = args[:schedule] || Schedule.new
-  end
+  include Schedulable
 
-  attr_reader :schedule, :size, :chain, :tire_size
-
-  def schedulable?(start_date, end_date)
-    !scheduled?(start_date - lead_days, end_date)
-  end
-
-  def scheduled?(start_date, end_date)
-    schedule.scheduled?(self, start_date, end_date)
-  end
+  attr_reader :size, :chain, :tire_size
 
   def lead_days
     1
