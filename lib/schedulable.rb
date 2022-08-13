d6495812608da6524b0a19e2d4f7987d5b47f1a0
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-13 11:23:08 +0900

    feat: add Schedulable

---
+++ lib/schedulable.rb
@@ -0,0 +1,17 @@
+module Schedulable
+  def schedule
+    @schedule ||= ::Schedule.new
+  end
+
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+
+  def lead_days
+    0
+  end
+end
