fb719fae69c5d7982e55a522b673309aa3b72f22
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-16 14:48:17 +0900

    feat: delegate spares to parts

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,9 +1,18 @@
 class Bicycle
   include Schedulable
 
-  attr_reader :size, :chain, :tire_size
+  def initialize(args={})
+    @size = args[:size]
+    @parts = args[:parts]
+  end
+
+  attr_reader :size, :parts
 
   def lead_days
     1
+  end
+
+  def spares
+    parts.spares
   end
 end
