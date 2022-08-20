4d351d993372e1c1a79471fe21cda3ae4f2696e2
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-20 11:35:25 +0900

    feat: add RoadBikeParts

---
+++ app/models/road_bike_parts.rb
@@ -0,0 +1,19 @@
+class RoadBikeParts < Parts
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    {
+      tape_color: tape_color
+    }
+  end
+
+  def default_tire_size
+    '23'
+  end
+
+  private
+
+  attr_reader :tape_color
+end
