0b60b6c310ad3ad18e8ab1336b91399acc88aab0
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-28 13:11:41 +0900

    refactor: remove unused part

--- app/models/part.rb
+++
@@ -1,9 +0,0 @@
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-end
