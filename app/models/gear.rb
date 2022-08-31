8a5aae675babf468ff4bb6e1c42a6ec1c3054cca
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-31 17:35:05 +0900

    feat: Gearの改修
    gear_inchesの桁数を安定させた

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -3,15 +3,15 @@
 class Gear
   attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(args)
-    @chainring = args[:chainring]
-    @cog = args[:cog]
+  def initialize(chainring:, cog:, rim:, tire:)
+    @chainring = chainring
+    @cog = cog
     @rim = rim
     @tire = tire
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    (ratio * Wheel.new(rim, tire).diameter).round(1)
   end
 
   def ratio
