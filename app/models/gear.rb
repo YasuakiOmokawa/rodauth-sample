6a1b06f525af1c8e06a11d4e2ad1067f751075d0
面川泰明 <omokawa@senk-inc.co.jp>
2022-08-31 16:36:17 +0900

    feat: add Gear

---
+++ app/models/gear.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @rim = rim
+    @tire = tire
+  end
+
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
