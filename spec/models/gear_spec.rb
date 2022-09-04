f8625bf2f23598899d6e81dbf57abbbbb5dac7ea
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-04 12:06:02 +0900

    spec: use instance_double
    diameterインタフェースが変化したときに定義元の情報から変化を検知できるようにした

--- spec/models/gear_spec.rb
+++ spec/models/gear_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 RSpec.describe Gear, type: :model do
   describe '#gear_inches' do
     it 'returns gear_inches' do
-      diameter_double = double('Diameterizable')
-      allow(diameter_double).to receive(:diameter).and_return(10)
+      diameter_double = instance_double(Wheel)
+      allow(diameter_double).to receive(:width).and_return(10)
       gear = Gear.new(chainring: 52, cog: 11, wheel: diameter_double)
       expect(gear.gear_inches).to eq 47.3
     end
