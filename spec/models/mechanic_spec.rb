6b19f82fce10684026b3b0e1c1263f7350d68b93
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-07 17:43:07 +0900

    spec: change to use shared_examples
    preparer.rbを使うよう修正

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -3,10 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
-  describe '#prepare_trip' do
-    it 'respond to this method' do
-      mechanic = Mechanic.new
-      expect(mechanic).to respond_to(:prepare_trip)
-    end
-  end
+  let(:preparer) { Mechanic.new }
+
+  it_behaves_like 'preparer'
 end
