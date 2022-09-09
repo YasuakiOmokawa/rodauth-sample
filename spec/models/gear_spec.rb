dccbf0f51a7e4c3116e07f2484a9cb8a65f6783e
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-09 16:57:21 +0900

    spec: gear_inchesはbe_withinでテスト
    doubleはDiameterizableの変化に対応できないが後で直す

--- spec/models/gear_spec.rb
+++ spec/models/gear_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Gear, type: :model do
   describe '#gear_inches' do
-    it 'returns gear_inches' do
-      diameter_double = instance_double(Wheel)
-      allow(diameter_double).to receive(:width).and_return(10)
-      gear = Gear.new(chainring: 52, cog: 11, wheel: diameter_double, observer: nil)
-      expect(gear.gear_inches).to eq 47.3
-    end
+    subject { gear.gear_inches }
+
+    let(:gear) { Gear.new(chainring: 11, cog: 10, wheel:, observer: nil) }
+    let(:wheel) { double('Wheel', width: 30) }
+
+    it { is_expected.to be_within(0.01).of(33.0) }
   end
 
   describe '#set_cog' do
