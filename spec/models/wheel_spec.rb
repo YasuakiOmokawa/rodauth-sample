b42e3068d2d46a86b0dab58cd20e8d7747c62b39
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-10 19:10:16 +0900

    spec: 大文字にしたshared_examplesの名前に合わせた

--- spec/models/wheel_spec.rb
+++ spec/models/wheel_spec.rb
@@ -5,5 +5,5 @@ require 'rails_helper'
 RSpec.describe Wheel, type: :model do
   let(:diameterizable) { described_class.new(26, 1.5) }
 
-  it_behaves_like 'diameterizable'
+  it_behaves_like 'Diameterizable'
 end
