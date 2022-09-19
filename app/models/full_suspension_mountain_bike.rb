83d2900022e1143739d4601fb456ca0b093e7c82
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-19 11:51:59 +0900

    feat: off_road_abilityを抽出

--- app/models/full_suspension_mountain_bike.rb
+++ app/models/full_suspension_mountain_bike.rb
@@ -2,14 +2,25 @@
 
 class FullSuspensionMountainBike
 
-  def initialize(type_code:, base_price:, commission:)
+  TIRE_WIDTH_FACTOR = 10
+  FRONT_SUSPENSION_FACTOR = 20
+  REAR_SUSPENSION_FACTOR = 30
+
+  def initialize(type_code:, base_price:, commission:, tire_width:, front_fork_travel:, rear_fork_travel:)
     @type_code = type_code
     @base_price = base_price
     @commission = commission
+    @tire_width = tire_width
+    @front_fork_travel = front_fork_travel
+    @rear_fork_travel = rear_fork_travel
   end
 
   def price
     (1 + commission) * base_price + front_suspension_price + rear_suspension_price
+  end
+
+  def off_road_ability
+    tire_width * TIRE_WIDTH_FACTOR + front_fork_travel * FRONT_SUSPENSION_FACTOR + rear_fork_travel * REAR_SUSPENSION_FACTOR
   end
 
   private
@@ -22,5 +33,5 @@ class FullSuspensionMountainBike
     100
   end
 
-  attr_reader :type_code, :base_price, :commission
+  attr_reader :type_code, :base_price, :commission, :tire_width, :front_fork_travel, :rear_fork_travel
 end
