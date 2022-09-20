95702fd785094131804f5979a9943df985025c88
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-20 16:31:04 +0900

    feat: add HtmlFormatter

---
+++ app/models/html_formatter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class HtmlFormatter
+  def output_report(context)
+    puts('<html>')
+    puts('  <head>')
+    puts("    <title>#{context.title}</title>")
+    puts('  </head>')
+    puts('  <body>')
+    context.text.each do |line|
+      puts("    <p>#{line}</p>")
+    end
+    puts('  </body>')
+    puts('</html>')
+  end
+end
