b4e39ee83641c334bb64ab8be8f5baa4dfea611d
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-22 12:03:45 +0900

    feat: full_suspension用の#off_road_abilityを作成

--- app/models/strategy_pattern/full_suspension_mountain_bike.rb
+++ app/models/strategy_pattern/full_suspension_mountain_bike.rb
@@ -3,5 +3,17 @@
 module StrategyPattern
   class FullSuspensionMountainBike
 
+    def initialize(params)
+      @tire_width = params[:tire_width]
+      @front_fork_travel = params[:front_fork_travel]
+      @rear_fork_travel = params[:rear_fork_travel]
+    end
+
+    def off_road_ability
+      @tire_width * StrategyPattern::MountainBike::TIRE_WITH_FACTOR +
+      @front_fork_travel * StrategyPattern::MountainBike::FRONT_SUSPENSION_FACTOR +
+      @rear_fork_travel * StrategyPattern::MountainBike::REAR_SUSPENSION_FACTOR
+    end
+
   end
 end
