e509d021da6921fe310b1b4943e198288a722cb3
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-22 11:35:15 +0900

    feat: #off_road_abilityを移譲できるように実装

--- app/models/strategy_pattern/rigid_mountain_bike.rb
+++ app/models/strategy_pattern/rigid_mountain_bike.rb
@@ -2,6 +2,13 @@
 
 module StrategyPattern
   class RigidMountainBike
-    
+
+    def initialize(params)
+      @tire_width = params[:tire_width]
+    end
+
+    def off_road_ability
+      @tire_width * StrategyPattern::MountainBike::TIRE_WITH_FACTOR
+    end
   end
 end
