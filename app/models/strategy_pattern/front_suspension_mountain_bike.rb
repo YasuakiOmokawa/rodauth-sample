3956e3f9e3953b962c079564051d87d611c30a16
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-23 10:58:15 +0900

    feat: move #price to FrontSuspensionMountainBike

--- app/models/strategy_pattern/front_suspension_mountain_bike.rb
+++ app/models/strategy_pattern/front_suspension_mountain_bike.rb
@@ -12,5 +12,9 @@ module StrategyPattern
       @tire_width * StrategyPattern::MountainBike::TIRE_WITH_FACTOR +
       @front_fork_travel * StrategyPattern::MountainBike::FRONT_SUSPENSION_FACTOR
     end
+
+    def price
+      (1 + @commission) * @base_price + @front_suspension_price
+    end
   end
 end
