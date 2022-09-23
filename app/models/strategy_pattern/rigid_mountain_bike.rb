98a761e9d4e6314c0d4654caa57c5807a5c8d7ae
面川泰明 <omokawa@senk-inc.co.jp>
2022-09-23 10:58:56 +0900

    feat: move #price to RigidMountainBike

--- app/models/strategy_pattern/rigid_mountain_bike.rb
+++ app/models/strategy_pattern/rigid_mountain_bike.rb
@@ -10,5 +10,9 @@ module StrategyPattern
     def off_road_ability
       @tire_width * StrategyPattern::MountainBike::TIRE_WITH_FACTOR
     end
+
+    def price
+      (1 + @commission) * @base_price
+    end
   end
 end
