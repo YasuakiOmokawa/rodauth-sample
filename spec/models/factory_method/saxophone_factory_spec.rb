81a2d50fdd0d28f15a66e1930bc67c461619a62d
面川泰明 <omokawa@senk-inc.co.jp>
2022-10-01 11:45:30 +0900

    spec: 抽象クラスのテストを削除して具象クラスのテストに変更

---
+++ spec/models/factory_method/saxophone_factory_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FactoryMethod::SaxophoneFactory, type: :model do
+  let(:saxophone_factory) { FactoryMethod::SaxophoneFactory.new(3) }
+
+  describe '#ship_out' do
+    context '1度出力するとアイテムの配列が空になる' do
+      before { saxophone_factory.ship_out }
+      specify do
+        expect(saxophone_factory.ship_out).to eq []
+      end
+    end
+
+    context '1回実施すると複数作られる' do
+      specify do
+        expect(saxophone_factory.ship_out.map(&:play)).to eq [
+          'sax: Instrument 0 is playing music.',
+          'sax: Instrument 1 is playing music.',
+          'sax: Instrument 2 is playing music.'
+        ]
+      end
+    end
+  end
+end
