cf1b7d2d481e4baac0cbe47282f019e2f301c175
面川泰明 <omokawa@senk-inc.co.jp>
2022-10-04 12:06:52 +0900

    feat: メソッドの参照先を記述

--- app/models/transaction/basic_application_model.rb
+++ app/models/transaction/basic_application_model.rb
@@ -5,8 +5,19 @@ module Transaction
     include ::ActiveModel::Model
     include ::ActiveModel::Validations::Callbacks
 
-    define_model_callbacks :save
+    define_model_callbacks :save # ActiveModel::Model -> ActiveModel::API -> ActiveModel::Validations -> ActiveModel::Callbacks#define_model_callbacks
 
-    before_validation :normalize # ::ActiveModel::Validations::Callbacksで定義
+    before_validation :normalize # ActiveModel::Validations::Callbacks#before_validation
+
+    private
+
+    # 入力値に対する正規化を実施する場合オーバーライドしてください
+    #
+    # example:
+    #   def normalize
+    #     self.name = normalize_name(name) if name.present?
+    #   end
+    #
+    def normalize; end
   end
 end
