e2d722bb909d4e6619ba5751977eaac97f0985d4
面川泰明 <omokawa@senk-inc.co.jp>
2022-10-08 12:44:46 +0900

    feat: add defile_model_callbacks
    [:create, :update, :destroy]

--- app/models/transaction/application_model.rb
+++ app/models/transaction/application_model.rb
@@ -4,5 +4,9 @@ module Transaction
   class ApplicationModel < BasicApplicationModel
     include DelegateAttribute
 
+    define_model_callbacks :create
+    define_model_callbacks :update
+    define_model_callbacks :destroy
+
   end
 end
