b6fa62e382d7e6a223b30cd7ef5a05222f221d88
面川泰明 <omokawa@senk-inc.co.jp>
2022-10-09 12:20:00 +0900

    feat: save時の複数モデルトランザクションを可能にした

--- app/models/transaction/application_model.rb
+++ app/models/transaction/application_model.rb
@@ -8,5 +8,35 @@ module Transaction
     define_model_callbacks :update
     define_model_callbacks :destroy
 
+    def initialize(attributes = {})
+      super(attributes)
+      @models = []
+    end
+
+    def save
+      return false if invalid?
+
+      run_callbacks(:commit) { save_in_transaction }.present?
+    end
+
+    private
+
+    attr_reader :models
+
+    def save_in_transaction
+      result = ::ActiveRecord::Base.transaction do
+        result = run_callbacks(:save) { save_models }
+        raise ActiveRecord::Rollback if result
+
+        true
+      end
+      result.present?
+    rescue StandardError => e
+      handle_rollback(e)
+    end
+
+    def save_models
+      models.to_a.map(&:save!)
+    end
   end
 end
