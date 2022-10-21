3ec749f1edc3ec1f3136e557d18659155797993c
面川泰明 <omokawa@senk-inc.co.jp>
2022-10-21 14:01:44 +0900

    feat: fetch_optionを追加

--- app/models/transaction/updating_context.rb
+++ app/models/transaction/updating_context.rb
@@ -4,7 +4,9 @@ module Transaction
   class UpdatingContext < ApplicationModel
     attribute :api, :boolean
 
-    def initialize
+    def initialize(options)
+      @options = options
+
       super(
         api: fetch_option_or_default(:api)
       )
@@ -12,16 +14,18 @@ module Transaction
 
     private
 
+    attr_reader :options
+
     def fetch_option_or_default(type)
-      if option == fetch_option(type)
+      if (option == fetch_option(type))
         option.quantity
       else
         master_data[type]
       end
     end
 
-    def option; end
-
-    def fetch_option(type); end
+    def fetch_option(type)
+      options.find { _1.option_type == type.to_s }
+    end
   end
 end
