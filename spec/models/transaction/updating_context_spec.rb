8e680ac440983e7bc334fd4385003d64d20377b9
面川泰明 <omokawa@senk-inc.co.jp>
2022-10-24 16:36:07 +0900

    spec: テスト用のプラン項目を変更

--- spec/models/transaction/updating_context_spec.rb
+++ spec/models/transaction/updating_context_spec.rb
@@ -3,23 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Transaction::UpdatingContext, type: :model do
-  subject(:context) { described_class.new(options) }
+  subject(:context) { described_class.new(options, master_plan) }
 
   describe '#attributes' do
     subject { context.attributes }
+
+    let(:master_plan) {}
 
     context 'with options' do
       let(:options) do
         defaults = { quantity: 1 }
         [
-          Transaction::Option.new(**defaults.merge(option_type: 'api')),
+          Transaction::Option.new(**defaults.merge(option_type: 'api_client')),
           Transaction::Option.new(**defaults.merge(option_type: 'pdf_upload_count', quantity: 10))
         ]
       end
 
       it 'optionsが反映されること' do
         is_expected.to eq({
-          api: true
+          api_client: true,
+          sms_contract: false
         }.stringify_keys)
       end
     end
