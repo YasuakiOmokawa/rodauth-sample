fd3c6ab09e8bd9d394a580a5e515ebba2123d111
面川泰明 <omokawa@senk-inc.co.jp>
2022-11-03 15:04:56 +0900

    feat: master_planのデフォルトデータを定義

--- app/models/master_plan.rb
+++ app/models/master_plan.rb
@@ -3,9 +3,17 @@
 class MasterPlan < MasterData
   extend Enumerize
 
+  DEFAULT_ID = 1 # plan_code : free
+
   enumerize :plan_code, in: %i[
     free
     pro
     light
   ]
+
+  class << self
+    def default
+      find(DEFAULT_ID)
+    end
+  end
 end
