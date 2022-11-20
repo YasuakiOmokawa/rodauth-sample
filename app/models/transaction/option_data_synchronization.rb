b176d724c23502228d3d5b458e969337c354f653
面川泰明 <omokawa@senk-inc.co.jp>
2022-11-20 09:02:36 +0900

    feat: implement update_and_destroy_option_data

--- app/models/transaction/option_data_synchronization.rb
+++ app/models/transaction/option_data_synchronization.rb
@@ -11,5 +11,18 @@ module Transaction
     def normalize
       update_and_destroy_option_data
     end
+
+    def update_and_destroy_option_data
+      option_datas.each do |option_data|
+        if (option = options.find { _1.license_type == option_data.license_type } )
+          option_data.assign_attributes(
+            plan_code: option.plan_code,
+            quantity: option.quantity,
+          )
+        else
+          option_data.mark_for_destruction
+        end
+      end
+    end
   end
 end
