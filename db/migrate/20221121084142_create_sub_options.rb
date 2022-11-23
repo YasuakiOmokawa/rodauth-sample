aa131abecc345b1773a1e97958acfe2041932677
面川泰明 <omokawa@senk-inc.co.jp>
2022-11-23 18:51:05 +0900

    change migration

--- db/migrate/20221121084142_create_sub_options.rb
+++ db/migrate/20221121084142_create_sub_options.rb
@@ -2,6 +2,11 @@
 
 class CreateSubOptions < ActiveRecord::Migration[7.0]
   def change
-    create_table :sub_options, &:timestamps
+    create_table :sub_options do |t|
+      t.string :option_type, null: false, comment: 'オプションの種類'
+      t.integer :quantity, null: false, comment: '数量'
+
+      t.timestamps
+    end
   end
 end
