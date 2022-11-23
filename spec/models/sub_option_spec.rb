ee5cebec7d306e025d1801c86bcdb5d91539651e
面川泰明 <omokawa@senk-inc.co.jp>
2022-11-23 18:52:53 +0900

    add notice of sub_options

--- spec/models/sub_option_spec.rb
+++ spec/models/sub_option_spec.rb
@@ -5,3 +5,14 @@ require 'rails_helper'
 RSpec.describe SubOption, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: sub_options
+#
+#  id          :integer          not null, primary key
+#  option_type :string           not null
+#  quantity    :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
