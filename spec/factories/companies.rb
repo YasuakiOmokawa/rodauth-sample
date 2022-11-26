6ee330a19272b1c788dd407c1e1838e3fd748aad
面川泰明 <omokawa@senk-inc.co.jp>
2022-11-26 12:50:13 +0900

    rails db:migrate

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -3,3 +3,14 @@ FactoryBot.define do
     
   end
 end
+
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :integer          not null, primary key
+#  address    :string           not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
