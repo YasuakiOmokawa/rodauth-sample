87595c88b2842b3727a5da9243d2f5fec455f7de
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-03 14:23:44 +0900

    コンストラクタでsub_optionを定義しないことが原因でエラーになることを実現

--- spec/models/transaction/sub_option_synchronization_spec.rb
+++ spec/models/transaction/sub_option_synchronization_spec.rb
@@ -3,12 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe Transaction::SubOptionSynchronization, type: :model do
-  describe '#save' do
-    let(:sub_option) { create(:sub_option) }
+  subject(:syncronization) { described_class.new() }
 
-    context 'some context' do
-      it 'some result' do
-        binding.b
+  describe '#create!' do
+    subject(:create!) { syncronization.create! }
+
+    let(:client) { double('Client') }
+    let(:company) { create(:company) }
+
+    context '連携したいオプションがある場合' do
+      before do
+        option_class = Transaction::Option
+        allow(client).to receive(:sub_options).and_return(
+          [
+            option_class.new(quantity: 1, option_type: 'api_client'),
+            option_class.new(quantity: 1, option_type: 'max_paper_upload_count'),
+          ],
+        )
+      end
+
+      context '同期対象でない場合' do
+        it 'オプションの情報が同期されない' do
+          expect { create! }.to_not change { company.reload.sub_options.size }
+        end
+      end
+
+      context '同期対象である場合' do
+        it 'オプションの情報が同期される' do
+
+        end
       end
     end
   end
