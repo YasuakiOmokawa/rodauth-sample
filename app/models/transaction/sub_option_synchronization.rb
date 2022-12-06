159993bffaa51e39ffe905deff620f81660b9001
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-06 14:08:56 +0900

    add build_new_sub_option

--- app/models/transaction/sub_option_synchronization.rb
+++ app/models/transaction/sub_option_synchronization.rb
@@ -15,6 +15,7 @@ module Transaction
 
     def normalize
       update_and_destroy_sub_option
+      build_new_sub_option
     end
 
     def update_and_destroy_sub_option
@@ -28,5 +29,18 @@ module Transaction
         end
       end
     end
+
+    def build_new_sub_option
+      option_types = sub_options.map(&:option_type)
+
+      api_sub_options.reject { _1.option_type.in?(option_types) }.each do |sub_option|
+        sub_options.build(
+          quantity: sub_option.quantity,
+          option_type: sub_option.option_type
+        )
+      end
+    end
+
+    def api_sub_options; end
   end
 end
