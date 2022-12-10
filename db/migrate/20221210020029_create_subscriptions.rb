4cfaa790461c4a8b309a75dc35b1aa1d4e3e4c7f
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-10 11:01:26 +0900

    rails g model Subscription company:references

---
+++ db/migrate/20221210020029_create_subscriptions.rb
@@ -0,0 +1,9 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subscriptions do |t|
+      t.references :company, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
