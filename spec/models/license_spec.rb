81eb93f52688fb262cb0e452b038396c60512a23
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-16 16:55:54 +0900

    exec migration

--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -17,14 +17,17 @@ end
 # Table name: licenses
 #
 #  id                :integer          not null, primary key
+#  licensable_type   :string
 #  next_payment_date :date             not null
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
+#  licensable_id     :bigint
 #  subscription_id   :integer          not null
 #
 # Indexes
 #
 #  index_licenses_on_subscription_id  (subscription_id)
+#  index_polymorphic_key_licenses     (licensable_type,licensable_id) UNIQUE
 #
 # Foreign Keys
 #
