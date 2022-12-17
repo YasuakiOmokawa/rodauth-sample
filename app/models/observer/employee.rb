3bd8b706b6d0758910af48c76f344af6efbfdd79
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-17 16:06:45 +0900

    notify_observer追加
    Observerを複数指定できるようにした

--- app/models/observer/employee.rb
+++ app/models/observer/employee.rb
@@ -4,16 +4,22 @@ module Observer
   class Employee
     attr_reader :name, :title, :salary
 
-    def initialize(name:, title:, salary:, payroll:)
+    def initialize(name:, title:, salary:)
       @name = name
       @title = title
       @salary = salary
-      @payroll = payroll
+      @observers = [Payroll.new]
     end
 
     def salary=(new_salary)
       @salary = new_salary
-      @payroll.update(self)
+      notify_observer
+    end
+
+    def notify_observer
+      @observers.each do |observer|
+        observer.update(self)
+      end
     end
   end
 end
