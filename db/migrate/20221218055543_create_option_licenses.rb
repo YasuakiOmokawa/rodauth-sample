b6cd89dda4d46ce4e7d00065601fb048e4526978
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-18 15:03:16 +0900

    fix rubocop

--- db/migrate/20221218055543_create_option_licenses.rb
+++ db/migrate/20221218055543_create_option_licenses.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateOptionLicenses < ActiveRecord::Migration[7.0]
   def change
     create_table :option_licenses do |t|
--- spec/factories/option_licenses.rb
+++ spec/factories/option_licenses.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :option_license do
-    
   end
 end
 
