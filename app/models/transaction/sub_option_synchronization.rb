0efe27cac0863d016d40afc5f2be66dc2c365a11
面川泰明 <omokawa@senk-inc.co.jp>
2022-12-22 11:03:55 +0900

    rename sub_option > option_license

--- app/models/transaction/sub_option_synchronization.rb
+++ app/models/transaction/sub_option_synchronization.rb
@@ -2,49 +2,49 @@
 
 module Transaction
   class SubOptionSynchronization < ApplicationModel
-    def initialize(company, client)
-      @company = company
+    def initialize(license, client)
+      @license = license
       @client = client
       super({})
-      @models << company
+      @models << license
     end
 
     private
 
-    attr_reader :company, :client
+    attr_reader :license, :client
 
-    delegate :sub_options, to: :company, private: true
+    delegate :option_licenses, to: :license, private: true
 
     def normalize
-      update_and_destroy_sub_option
-      build_new_sub_option
+      update_and_destroy_option_license
+      build_new_option_license
     end
 
-    def update_and_destroy_sub_option
-      sub_options.each do |sub_option|
-        if (option = api_sub_options.find { _1.option_type == sub_option.option_type })
-          sub_option.assign_attributes(
-            quantity: api_sub_options.quantity
+    def update_and_destroy_option_license
+      option_licenses.each do |option_license|
+        if (option = api_option_license.find { _1.license_type == option_license.license_type })
+          option_license.assign_attributes(
+            quantity: api_option_license.quantity
           )
         else
-          sub_option.mark_for_destruction
+          option_license.mark_for_destruction
         end
       end
     end
 
-    def build_new_sub_option
-      option_types = sub_options.map(&:option_type)
+    def build_new_option_license
+      license_types = option_license.map(&:license_type)
 
-      api_sub_options.reject { _1.option_type.in?(option_types) }.each do |sub_option|
-        sub_options.build(
-          quantity: sub_option.quantity,
-          option_type: sub_option.option_type
+      api_option_license.reject { _1.license_type.in?(license_types) }.each do |api_option|
+        option_licenses.build(
+          quantity: api_option.quantity,
+          license_type: api_option.license_type
         )
       end
     end
 
-    def api_sub_options
-      @api_sub_options = client.sub_options
+    def api_option_license
+      @api_option_license = client.option_license
     end
   end
 end
