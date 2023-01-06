cef2ff8a01046b5fb45f153fb2a44a8e511d9f25
面川泰明 <omokawa@senk-inc.co.jp>
2023-01-06 19:45:54 +0900

    基底コンポジットクラスを継承するようにした

--- app/models/composite/make_batter_task.rb
+++ app/models/composite/make_batter_task.rb
@@ -1,28 +1,12 @@
 # frozen_string_literal: true
 
 module Composite
-  class MakeBatterTask < Task
+  class MakeBatterTask < CompositeTask
     def initialize
       super('Make butter')
-      @sub_tasks = []
       add_sub_task(AddDryIngredientsTask.new)
       add_sub_task(AddLiquidsTask.new)
       add_sub_task(MixTask.new)
-    end
-
-    def add_sub_task(task)
-      @sub_tasks << task
-    end
-
-    def remove_sub_tasks(task)
-      @sub_tasks.delete(task)
-    end
-
-    # バターをつくっていきまーす
-    def get_time_required
-      time = 0.0
-      @sub_tasks.each { |task| time += task.get_time_required }
-      time
     end
   end
 end
