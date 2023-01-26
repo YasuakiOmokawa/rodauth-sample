63f119cc0d252c6ab9e269d213091f63dc32cbf1
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-01-26 21:13:18 +0900

    Impl BankAccount

---
+++ app/models/proxy/bank_account.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Proxy
+  class BankAccount
+    attr_reader :balance
+
+    def initialize(starting_balance = 0)
+      @balance = starting_balance
+    end
+
+    def deposit(amount)
+      @balance += amount
+    end
+
+    def withdraw(amount)
+      @balance -= amount
+    end
+  end
+end
