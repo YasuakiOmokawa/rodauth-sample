627ce142b0474a1663da0170eefabec417375ea4
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-01-26 21:13:28 +0900

    Impl BankAccountProxy

---
+++ app/models/proxy/bank_account_proxy.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Proxy
+  class BankAccountProxy
+    def initialize(real_object)
+      @real_object = real_object
+    end
+
+    def balance
+      @real_object.balance
+    end
+
+    def deposit(amount)
+      @real_object.deposit(amount)
+    end
+
+    def withdraw(amount)
+      @real_object.withdraw(amount)
+    end
+  end
+end
