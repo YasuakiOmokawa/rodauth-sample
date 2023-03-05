f956ff4e886253da21510375ef6164cc500f64b5
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-03-05 19:31:59 +0900

    create new_animal interface

--- app/models/factory/pond.rb
+++ app/models/factory/pond.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class Factory::Pond
-  def initialize(number_ducks)
-    @ducks = []
-    number_ducks.times do |i|
-      duck = Factory::Duck.new("duck #{i}")
-      @ducks << duck
+  def initialize(number_animals)
+    @animals = []
+    number_animals.times do |i|
+      animal = new_animal("animal #{i}")
+      @animals << animal
     end
   end
 
   def simulate_one_day
-    @ducks.each { |duck| duck.speak }
-    @ducks.each { |duck| duck.eat }
-    @ducks.each { |duck| duck.sleep }
+    @animals.each { |animal| animal.speak }
+    @animals.each { |animal| animal.eat }
+    @animals.each { |animal| animal.sleep }
   end
 end
