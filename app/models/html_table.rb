02891f3ee1a0356c8049aecf1fec93d5e062ae7d
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-04-24 20:12:55 +0900

    to_sのメモリ使用効率を改善

--- app/models/html_table.rb
+++ app/models/html_table.rb
@@ -32,20 +32,29 @@ class HtmlTable
   end
 
   def to_s
-    Table.new(
-      Tbody.new(
-        rows.map do |row|
-          Tr.new(
-            row.map do |cell|
-              Td.new(CGI.escapeHTML(cell.to_s))
-            end.join,
-          )
-        end.join,
-      ),
-    ).to_s
+    html = +''
+    wrap(html, 'table') do
+      wrap(html, 'tbody') do
+        rows.each do |row|
+          wrap(html, 'tr') do
+            row.each do |cell|
+              wrap(html, 'td') do
+                html << CGI.escapeHTML(cell.to_s)
+              end
+            end
+          end
+        end
+      end
+    end
   end
 
   private
 
   attr_reader :rows
+
+  def wrap(html, type)
+    html << '<' << type << '>'
+    yield
+    html << '</' << type << '>'
+  end
 end
