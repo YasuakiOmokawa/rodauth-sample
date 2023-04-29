52441a8d5293dff47f50a7bf4931d717eb0be389
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-04-29 14:35:06 +0900

    Impl TimeFilter

---
+++ app/models/time_filter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class TimeFilter
+  def initialize(start, finish)
+    @start = start
+    @finish = finish
+  end
+
+  def to_proc
+    proc do |time|
+      next false if start && time < start
+      next false if finish && time > finish
+
+      true
+    end
+  end
+
+  private
+
+  attr_reader :start, :finish
+end
